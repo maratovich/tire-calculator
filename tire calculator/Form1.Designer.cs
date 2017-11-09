b42c10ba51b6ce9f69396b7ddb15e6a97b026735
n.aidar <n.aidar@gmail.com>
2017-11-09 15:09:27 +0300

    Добавьте файлы проекта.

---
+++ tire calculator/Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace tire_calculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.wheelSizeComboBox = new System.Windows.Forms.ComboBox();
+            this.widthComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.profileComboBox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.newWheelSizeComboBox = new System.Windows.Forms.ComboBox();
+            this.newWidthComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.newProfileComboBox = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(8, 152);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(391, 141);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Показатель";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Прежний размер";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Новый размер";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Разница";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.wheelSizeComboBox);
+            this.groupBox1.Controls.Add(this.widthComboBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.profileComboBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(7, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(392, 64);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Старая размерность";
+            // 
+            // wheelSizeComboBox
+            // 
+            this.wheelSizeComboBox.FormattingEnabled = true;
+            this.wheelSizeComboBox.Location = new System.Drawing.Point(260, 33);
+            this.wheelSizeComboBox.Name = "wheelSizeComboBox";
+            this.wheelSizeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.wheelSizeComboBox.TabIndex = 19;
+            // 
+            // widthComboBox
+            // 
+            this.widthComboBox.FormattingEnabled = true;
+            this.widthComboBox.Location = new System.Drawing.Point(6, 33);
+            this.widthComboBox.Name = "widthComboBox";
+            this.widthComboBox.Size = new System.Drawing.Size(121, 21);
+            this.widthComboBox.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Посадочный диаметр";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Ширина профиля";
+            // 
+            // profileComboBox
+            // 
+            this.profileComboBox.FormattingEnabled = true;
+            this.profileComboBox.Location = new System.Drawing.Point(133, 33);
+            this.profileComboBox.Name = "profileComboBox";
+            this.profileComboBox.Size = new System.Drawing.Size(121, 21);
+            this.profileComboBox.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Высота профиля";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.newWheelSizeComboBox);
+            this.groupBox2.Controls.Add(this.newWidthComboBox);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.newProfileComboBox);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(7, 82);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(392, 64);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Новая размерность";
+            // 
+            // newWheelSizeComboBox
+            // 
+            this.newWheelSizeComboBox.FormattingEnabled = true;
+            this.newWheelSizeComboBox.Location = new System.Drawing.Point(260, 33);
+            this.newWheelSizeComboBox.Name = "newWheelSizeComboBox";
+            this.newWheelSizeComboBox.Size = new System.Drawing.Size(121, 21);
+            this.newWheelSizeComboBox.TabIndex = 19;
+            // 
+            // newWidthComboBox
+            // 
+            this.newWidthComboBox.FormattingEnabled = true;
+            this.newWidthComboBox.Location = new System.Drawing.Point(6, 33);
+            this.newWidthComboBox.Name = "newWidthComboBox";
+            this.newWidthComboBox.Size = new System.Drawing.Size(121, 21);
+            this.newWidthComboBox.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(260, 17);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Посадочный диаметр";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ширина профиля";
+            // 
+            // newProfileComboBox
+            // 
+            this.newProfileComboBox.FormattingEnabled = true;
+            this.newProfileComboBox.Location = new System.Drawing.Point(133, 33);
+            this.newProfileComboBox.Name = "newProfileComboBox";
+            this.newProfileComboBox.Size = new System.Drawing.Size(121, 21);
+            this.newProfileComboBox.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(133, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Высота профиля";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(416, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(298, 277);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 401);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "MainForm";
+            this.Text = "Tire calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox wheelSizeComboBox;
+        private System.Windows.Forms.ComboBox widthComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox profileComboBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox newWheelSizeComboBox;
+        private System.Windows.Forms.ComboBox newWidthComboBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox newProfileComboBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ tire calculator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tire_calculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            //Заполняем все 6-ть полей ComboBox данными: ширина профиля, высота и посадочный диаметр
+            //В последеющем заполнять данные из XML файла.
+            for (int i = 115; i < 365; i += 10)
+            {
+                widthComboBox.Items.Add(i);
+                newWidthComboBox.Items.Add(i);
+            }
+            for (int i = 25; i < 95; i += 5)
+            {
+                profileComboBox.Items.Add(i);
+                newProfileComboBox.Items.Add(i);
+            }
+            for (int i = 10; i < 24; i++)
+            {
+                wheelSizeComboBox.Items.Add(i);
+                newWheelSizeComboBox.Items.Add(i);
+            }
+        }
+    }
+}
